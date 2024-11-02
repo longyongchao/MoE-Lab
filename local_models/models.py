fix: 增加SG-MoE的Tok K softmax机制

local_models/models.py
@@ -167,10 +167,22 @@ class MoE(nn.Module):
             # 为避免除以 0，添加一个小常数 (epsilon)
             gate_outputs = gate_outputs + zero_row_mask * 1e-10
             
-        # 根据门控概率随机选择一个专家
-        # 重新归一化门控概率
-        gate_outputs = torch.softmax(gate_outputs, dim=1)
-        expert_indices = torch.multinomial(gate_outputs, num_samples=self.top_k).squeeze()  # 形状: [batch_size]
+        # 获取每个样本的前 top_k 个最大值和对应的索引
+        topk_values, topk_indices = torch.topk(gate_outputs, self.top_k, dim=1)
+
+        # 对 top_k 的最大值进行 softmax 归一化
+        topk_softmax_values = torch.softmax(topk_values, dim=1)
+
+        # 构造一个与 gate_outputs 形状相同的全零矩阵
+        topk_gate_outputs = torch.zeros_like(gate_outputs)
+
+        # 将 softmax 后的 top_k 值放入对应的位置
+        topk_gate_outputs.scatter_(1, topk_indices, topk_softmax_values)
+
+        # 使用归一化后的 gate 输出进行后续操作
+        gate_outputs = topk_gate_outputs
+        
+        expert_indices = torch.multinomial(gate_outputs, num_samples=1).squeeze()  # 形状: [batch_size]
 
         # 获取每个专家的输出
         expert_outputs = torch.stack([expert(x_flat) for expert in self.experts], dim=1)  # 形状: [batch_size, num_experts, output_dim]

train.py
@@ -4,8 +4,8 @@ import torch.optim as optim
 from local_models import models as Model
 
 # 切换数据集
-# from local_datasets import mnist_family as dataset
-from local_datasets import rgb_mix as dataset
+from local_datasets import mnist_family as dataset
+# from local_datasets import rgb_mix as dataset
 
 from evaluation import test
 from local_trainer import trainer as Trainer
@@ -27,13 +27,14 @@ datasets_info = dataset.get_datasets_info()
 
 expert_hidden_dim = [] # 专家隐藏层维度，如果需要单线性层，则设置[]
 gating_hidden_dim = [50] # 门控隐藏层维度，如果需要单线性层，则设置[]
-num_experts = 8
+num_experts = 4
 DMoE_margin_threshold = 0.5
 SGMoE_w_importance = 0.1
 
 enable_hard_constraint_DMoE = False
 enable_sparsely_gated_noise_SGMoE = True
 enable_soft_constraint_SGMoE = True
+top_k = 2
 
 batch_size = 64
 lr = 0.0005
@@ -48,7 +49,7 @@ num_epochs = (m, n)是两阶段的训练方式：
 # num_epochs = (2, 8)
 # num_epochs = (4, 6)
 # num_epochs = (8, 2)
-num_epochs = (15, 0)
+num_epochs = (10, 0)
 
 expert_dim = f"expert784-{expert_hidden_dim}-20_"
 gating_dim = f"gating784-{gating_hidden_dim}-{num_experts}_"
@@ -76,6 +77,7 @@ moe_model = Model.MoE(
     enable_hard_constraint=enable_hard_constraint_DMoE,
     enable_sparsely_gated_noise=enable_sparsely_gated_noise_SGMoE,
     enable_soft_constraint=enable_soft_constraint_SGMoE,
+    top_k=top_k
 ).to(device)
 
 